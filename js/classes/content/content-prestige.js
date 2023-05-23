7ef92e959af5e3b8efeaa28a961a0de2fa760c46
Aripander088e2 <121055226+Aripander088e2@users.noreply.github.com>
2023-05-23 15:50:26 -0400

    Update content-prestige.js

--- js/classes/content/content-prestige.js
+++ js/classes/content/content-prestige.js
@@ -10,10 +10,10 @@ class ContentPrestige {
                     return new Decimal(2).mul(level + 1).mul(level > 10 ? Decimal.pow(1.1, level - 10) : 1);
                 },
                 level => {
-                    return new Decimal(1 + 0.12 * level);
+                    return new Decimal(1 + 0.20 * level);
                 }, {
                 getEffectDisplay: effectDisplayTemplates.percentStandard("", 1),
-                maxLevel: 2500
+                maxLevel: 25
             }),
             matterBoost: new PrestigeUpgrade("Matter Boost", "Boost Matter Production even further.",
                 level => {
